ad9b5a61506d3f8ed5b2b3446f8a264299f0fc74
Christoph Malek <malekchristoph@gmail.com>
2013-12-22 10:26:11 +0100

    added common.hpp

---
+++ include/rectojump/global/common.hpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2013 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_GLOBAL_COMMON_HPP
+#define RJ_GLOBAL_COMMON_HPP
+
+
+#include <SFML/Window.hpp>
+
+
+namespace rj
+{
+	using dur = float;
+
+	using key = sf::Keyboard::Key;
+	using btn = sf::Mouse::Button;
+}
+
+
+#endif // RJ_GLOBAL_COMMON_HPP
