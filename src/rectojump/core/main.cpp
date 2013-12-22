6133f50da9b0aee8b218bf21b80ecc0ea4ed171f
Christoph Malek <malekchristoph@gmail.com>
2013-12-22 09:29:38 +0100

    main.cpp

--- src/rectojump/core/main.cpp
+++ src/rectojump/core/main.cpp
@@ -3,8 +3,17 @@
 // See LICENSE for more information.
 //
 
+
+#include <rectojump/core/game.hpp>
+#include <rectojump/core/game_window.hpp>
+
+
 int main()
 {
+	rj::game_window gw;
+	rj::game g{gw};
+
+	gw.start();
 
 	return 0;
 }
