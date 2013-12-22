b52f0cfc22840c737f4cbf578dd8f07962a611b0
Christoph Malek <malekchristoph@gmail.com>
2013-12-22 09:28:03 +0100

    gamewindow

---
+++ include/rectojump/core/game_window.hpp
@@ -0,0 +1,95 @@
+﻿//
+// Copyright (c) 2013 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_CORE_GAME_WINDOW_HPP
+#define RJ_CORE_GAME_WINDOW_HPP
+
+
+#include "game_updater.hpp"
+#include <rectojump/shared/input.hpp>
+
+#include <mlk/log/log.h>
+#include <mlk/signals_slots/slot.h>
+#include <mlk/time/time.h>
+#include <mlk/types/types.h>
+
+#include <SFML/Graphics.hpp>
+
+
+namespace rj
+{
+	class game_window
+	{
+		mlk::uint m_width{800}, m_height{600};
+		sf::RenderWindow m_window;
+		bool m_running{false};
+
+		input m_input;
+
+		game_updater m_game_updater;
+
+	public:
+		mlk::slot<> on_stop;
+
+		game_window() :
+			m_window{{m_width, m_height}, "RectoJump"}
+		{m_input.on_key_pressed[key::Escape] += [this]{this->stop();};}
+
+		// interface
+		void start()
+		{
+			if(m_running) return;
+
+			this->prepare_start();
+			while(m_running)
+			{
+				m_game_updater.start_pt();
+
+				// update
+				this->update_events();
+				m_game_updater.update();
+
+				// render
+				m_window.clear();
+				m_game_updater.render();
+				m_window.display();
+
+				m_game_updater.end_pt();
+			}
+		}
+
+		void stop() noexcept
+		{m_running = false; on_stop();}
+
+		void draw(const sf::Drawable& object)
+		{m_window.draw(object);}
+
+		// getters
+		game_updater& updater() noexcept
+		{return m_game_updater;}
+
+	private:
+		void prepare_start() noexcept
+		{
+			m_running = true;
+			mlk::lout("rj::game_window", true) << "starting gamewindow";
+		}
+
+		void update_events() noexcept
+		{
+			sf::Event ev;
+			while(m_window.pollEvent(ev))
+			{
+				if(ev.type == sf::Event::EventType::Closed)
+					this->stop();
+				if(ev.type == sf::Event::EventType::KeyPressed)
+					m_input.key_pressed(ev.key.code);
+			}
+		}
+	};
+}
+
+
+#endif // RJ_CORE_GAME_WINDOW_HPP
