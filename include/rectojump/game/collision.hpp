4b21559ea276b9f9eb0b25fe01ef02e7cf1bf542
Christoph Malek <malekchristoph@gmail.com>
2013-12-27 12:24:50 +0100

    collision

---
+++ include/rectojump/game/collision.hpp
@@ -0,0 +1,23 @@
+//
+// Copyright (c) 2013 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_GAME_COLLISION_HPP
+#define RJ_GAME_COLLISION_HPP
+
+
+namespace rj
+{
+	template<typename T, typename E>
+	bool is_colliding(const T& o1, const E& o2)
+	{
+		return o1.right_out() >= o2.left_out() &&
+				o1.left_out() <= o2.right_out() &&
+				o1.bottom_out() >= o2.top_out() &&
+				o1.top_out() <= o2.bottom_out();
+	}
+}
+
+
+#endif // RJ_GAME_COLLISION_HPP
