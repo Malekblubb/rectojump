21b08245b796f4e43a2189f1dfbc6b221972c5ab
Christoph Malek <malekchristoph@gmail.com>
2013-12-30 15:01:54 +0100

    config_parser

---
+++ include/rectojump/global/config_parser.hpp
@@ -0,0 +1,52 @@
+//
+// Copyright (c) 2013 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_GLOBAL_CONFIG_PARSER_HPP
+#define RJ_GLOBAL_CONFIG_PARSER_HPP
+
+
+#include <string>
+#include <utility>
+#include <vector>
+#include <iostream>
+
+namespace rj
+{
+	using config_entry = std::pair<std::string, std::string>;
+	using config_entry_vec = std::vector<config_entry>;
+
+	class config_parser
+	{
+		const std::vector<std::string>& m_content;
+		config_entry_vec m_entrys;
+
+	public:
+		config_parser(const std::vector<std::string>& content) :
+			m_content{content}
+		{this->parse();}
+
+		auto get_entrys() const noexcept
+		-> const decltype(m_entrys)&
+		{return m_entrys;}
+
+	private:
+		void parse() noexcept
+		{
+			for(auto& a : m_content)
+			{
+				auto pos_equ(a.find('='));
+				if(pos_equ != std::string::npos)
+				{
+					auto pair(std::make_pair(a.substr(0, pos_equ), a.substr(pos_equ + 1, a.size() - pos_equ - 1)));
+					if(!pair.first.empty() && !pair.second.empty())
+						m_entrys.push_back(pair);
+				}
+			}
+		}
+	};
+}
+
+
+#endif // RJ_GLOBAL_CONFIG_PARSER_HPP
