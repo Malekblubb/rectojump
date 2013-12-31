cc9f8e96644f0d392cd19e6134fbf7c783ba5fde
Christoph Malek <malekchristoph@gmail.com>
2013-12-31 12:34:13 +0100

    added factory-wrapper function

---
+++ include/rectojump/game/factory.hpp
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2013 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_GAME_FACTORY_HPP
+#define RJ_GAME_FACTORY_HPP
+
+
+#include <memory>
+
+
+namespace rj
+{
+	namespace factory
+	{
+		template<typename T>
+		using eptr = std::shared_ptr<T>;
+
+		template<typename T, typename... Args>
+		eptr<T> create(Args&&... args)
+		{return std::make_shared<T>(std::forward<Args>(args)...);}
+	}
+}
+
+
+
+#endif // RJ_GAME_FACTORY_HPP
