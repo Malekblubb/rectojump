b7f98e38c0a4bd7339b5b2292c250bbbd19fa3fc
Christoph Malek <malekchristoph@gmail.com>
2014-01-02 18:55:59 +0100

    main menu raw class

---
+++ include/rectojump/core/main_menu.hpp
@@ -0,0 +1,29 @@
+//
+// Copyright (c) 2013 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_CORE_MAIN_MENU_HPP
+#define RJ_CORE_MAIN_MENU_HPP
+
+
+#include <rectojump/global/common.hpp>
+
+
+namespace rj
+{
+	class main_menu
+	{
+
+	public:
+		main_menu()
+		{ }
+
+
+		void update(dur duration)
+		{ }
+	};
+}
+
+
+#endif // RJ_CORE_MAIN_MENU_HPP
