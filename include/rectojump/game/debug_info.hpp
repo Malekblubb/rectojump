390e3224af03aa63dbbc5267466e366e3a70b030
Christoph Malek <malekchristoph@gmail.com>
2014-01-02 18:14:56 +0100

    added debug_info

---
+++ include/rectojump/game/debug_info.hpp
@@ -0,0 +1,42 @@
+//
+// Copyright (c) 2013 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_GAME_DEBUG_INFO_HPP
+#define RJ_GAME_DEBUG_INFO_HPP
+
+
+#include <rectojump/global/common.hpp>
+#include <rectojump/shared/debug_text.hpp>
+
+#include <mlk/tools/stl_string_utl.h>
+
+
+namespace rj
+{
+	class game;
+
+	class debug_info
+	{
+		game& m_game;
+		sf::RectangleShape m_background{{100.f, 100.f}};
+		debug_text m_text{"debug_font.png"_data};
+
+	public:
+		debug_info(game& g) :
+			m_game{g}
+		{
+			m_background.setPosition({1.f, 1.f});
+			m_background.setFillColor({255, 150, 123, 100});
+			m_background.setOutlineThickness(1);
+			m_background.setOutlineColor({255, 0, 0});
+		}
+
+		void update(dur);
+		void render();
+	};
+}
+
+
+#endif // RJ_GAME_DEBUG_INFO_HPP
---
+++ include/rectojump/game/debug_info.inl
@@ -0,0 +1,33 @@
+//
+// Copyright (c) 2013 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_GAME_DEBUG_INFO_INL
+#define RJ_GAME_DEBUG_INFO_INL
+
+
+#include "debug_info.hpp"
+
+
+namespace rj
+{
+	void debug_info::update(dur)
+	{
+		m_background.setSize(m_text.get_size());
+		auto text(mlk::stl_string::str_format("FPS:      %%\nFDur:     %%\nEntities: %%",
+				  m_game.get_updater().get_fps(),
+				  m_game.get_updater().get_frameduration(),
+				  m_game.get_world().num_entities()));
+		m_text.set_text(text);
+	}
+
+	void debug_info::render()
+	{
+		m_game.render_object(m_background);
+		m_game.render_object(m_text);
+	}
+}
+
+
+#endif // RJ_GAME_DEBUG_INFO_INL
