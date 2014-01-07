31d28fd217a8c4a601d92ac3b92eeaab8114ed19
Christoph Malek <malekchristoph@gmail.com>
2014-01-07 21:53:05 +0100

    (VERY WIP) packer base

---
+++ include/rectojump/shared/level_manager/packer_decl.hpp
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2013-2014 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_SHARED_LEVEL_MANAGER_PACKER_DECL_HPP
+#define RJ_SHARED_LEVEL_MANAGER_PACKER_DECL_HPP
+
+
+namespace rj
+{
+	enum class packer_mode : char
+	{pack, unpack};
+
+	template<packer_mode mode>
+	class level_packer;
+}
+
+
+#endif // RJ_SHARED_LEVEL_MANAGER_PACKER_DECL_HPP
