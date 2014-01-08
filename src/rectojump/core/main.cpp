d3aa28d7bbd2d813769e6f68f84cbbc8dba29a65
Christoph Malek <malekchristoph@gmail.com>
2014-01-08 18:57:22 +0100

    main adaption

--- src/rectojump/core/main.cpp
+++ src/rectojump/core/main.cpp
@@ -7,8 +7,9 @@
 #include <rectojump/core/game_window.hpp>
 #include <rectojump/core/main_menu.hpp>
 #include <rectojump/core/state.hpp>
-#include <rectojump/global/error_inserter.hpp>
 #include <rectojump/game/debug_info.inl>
+#include <rectojump/global/error_inserter.hpp>
+#include <rectojump/shared/data_manager.hpp>
 
 
 int main()
@@ -16,13 +17,15 @@ int main()
 	// generate errors
 	rj::error_inserter{};
 
+	// data
+	rj::data_manager dm{rj::data_path, true};
+
 	// game
 	rj::game_window gw;
 	rj::game g{gw};
 	rj::main_menu m{g};
-	rj::state_handler sh{gw, g, m};
+	rj::state_handler sh{gw, g, m, dm};
 
 	gw.start();
 	return 0;
 }
-
