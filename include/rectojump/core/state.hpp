f84e8bbd68cce80442f1d41e51cca000161c238b
Christoph Malek <malekchristoph@gmail.com>
2014-01-08 18:57:08 +0100

    state adaption

--- include/rectojump/core/state.hpp
+++ include/rectojump/core/state.hpp
@@ -10,6 +10,7 @@
 #include "game.hpp"
 #include "game_window.hpp"
 #include "main_menu.hpp"
+#include <rectojump/shared/data_manager.hpp>
 
 #include <mlk/containers/container_utl.h>
 #include <mlk/tools/bitset.h>
@@ -18,66 +19,91 @@
 namespace rj
 {
 	enum class state : char
-	{main_menu, game_menu, game};
+	{main_menu, game_menu, game, debug_info, num};
 
 	class state_handler
 	{
 		game_window& m_game_window;
 		game& m_game;
-		main_menu& m_menu;
+		main_menu& m_main_menu;
+		data_manager& m_datamgr;
+
 		debug_info m_debug_info;
 
-		static constexpr std::size_t m_num_states{3};
-		mlk::bitset<state, m_num_states> m_current_state;
-		std::map<state, mlk::slot<dur>> m_states;
-
+		mlk::bitset<state, mlk::enum_utl::to_int<std::size_t>(state::num)> m_current_states;
 
 	public:
-		state_handler(game_window& window, game& g, main_menu& menu) :
+		state_handler(game_window& window, game& g, main_menu& menu, data_manager& datamgr) :
 			m_game_window{window},
 			m_game{g},
-			m_menu{menu},
+			m_main_menu{menu},
+			m_datamgr{datamgr},
 			m_debug_info{m_game}
 		{
-//			m_current_state |= state::main_menu;
-			m_current_state |= state::game;
-			m_states[state::game] += [this](dur duration){m_game.update(duration); m_game.render();};
-			m_states[state::main_menu] += [this](dur duration){m_menu.update(duration);};
+			m_current_states |= state::game;
 
 			m_game_window.get_updater().on_update += [this](dur duration){this->update(duration);};
 			m_game_window.get_updater().on_render += [this]{this->render();};
 
+
+			on_keys_pressed(key::LShift, key::D) +=
+			[this]{this->toggle_state(state::debug_info);};
+
 			on_key_pressed(key::P) +=
 			[this]
 			{
-				if(m_current_state & state::game)
-				{this->set_only_state(state::game_menu); return;}
+				if(m_current_states & state::game)
+				{
+					m_current_states.remove(state::game);
+					m_current_states |= state::game_menu;
+					return;
+				}
 
-				if(m_current_state & state::game_menu)
-				{this->set_only_state(state::game); return;}
+				if(m_current_states & state::game_menu)
+				{
+					m_current_states.remove(state::game_menu);
+					m_current_states |= state::game;
+					return;
+				}
 			};
 		}
 
 	private:
+		void toggle_state(state s)
+		{m_current_states.toggle(s);}
+
+		bool is_state_activated(state s) const noexcept
+		{return m_current_states & s;}
+
 		void set_only_state(state s)
 		{
-			m_current_state.remove_all();
-			m_current_state |= s;
+			m_current_states.remove_all();
+			m_current_states |= s;
 		}
 
 		void update(dur duration)
 		{
-			for(auto& a : m_states)
-				if(m_current_state & a.first)
-					a.second(duration);
+			if(this->is_state_activated(state::game))
+				m_game.update(duration);
 
-			m_debug_info.update(duration);
+			else if(this->is_state_activated(state::main_menu))
+				m_main_menu.update(duration);
+
+			if(this->is_state_activated(state::debug_info))
+				m_debug_info.update(duration);
 		}
 
 		void render()
 		{
-			m_game.render();
-			m_debug_info.render();
+			// render game when in game menu
+			if(this->is_state_activated(state::game) || this->is_state_activated(state::game_menu))
+				m_game.render();
+
+			else if(this->is_state_activated(state::main_menu))
+				m_main_menu.render();
+
+			if(this->is_state_activated(state::debug_info))
+				m_debug_info.render();
 		}
 	};
 }
