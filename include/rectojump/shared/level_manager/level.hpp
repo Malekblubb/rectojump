2d11558403a352d97340d9322dad94214d35cde4
Christoph Malek <malekchristoph@gmail.com>
2014-01-09 19:15:19 +0100

    fixed misspelling

--- include/rectojump/shared/level_manager/level.hpp
+++ include/rectojump/shared/level_manager/level.hpp
@@ -24,11 +24,11 @@ namespace rj
 	struct level
 	{
 		music_data music;
-		entity_proto_vec entites;
+		entity_proto_vec entities;
 		level_info info;
 
 		bool is_valid() const noexcept
-		{return !music.empty() && !entites.empty();}
+		{return !music.empty() && !entities.empty();}
 	};
 }
 
