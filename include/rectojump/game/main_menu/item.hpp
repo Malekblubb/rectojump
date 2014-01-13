1d1a380c0fae26a9435e9d4d8d4270c31ec23b7a
Christoph Malek <malekchristoph@gmail.com>
2014-01-13 20:30:44 +0100

    menu refactor: item

---
+++ include/rectojump/game/main_menu/item.hpp
@@ -0,0 +1,30 @@
+//
+// Copyright (c) 2013-2014 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_CORE_MAIN_MENU_ITEM_HPP
+#define RJ_CORE_MAIN_MENU_ITEM_HPP
+
+
+#include <string>
+
+
+namespace rj
+{
+	using item_id = std::string;
+
+	struct item
+	{
+		item_id id;
+		std::size_t index;
+		bool is_valid() const noexcept
+		{return !id.empty();}
+	};
+
+	bool operator<(const item& lhs, const item&rhs)
+	{return lhs.index < rhs.index;}
+}
+
+
+#endif // RJ_CORE_MAIN_MENU_ITEM_HPP
