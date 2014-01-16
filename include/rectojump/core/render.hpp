a7fa348ea3fced3f7601669ad1968ee31fd112ee
Christoph Malek <malekchristoph@gmail.com>
2014-01-16 19:12:09 +0100

    added rj::rndr::rmo<T, T...>(...): render multiple objects in one fnc

--- include/rectojump/core/render.hpp
+++ include/rectojump/core/render.hpp
@@ -19,6 +19,21 @@ namespace rj
 		template<typename Game>
 		void ro(Game& g, const sf::Drawable& drawable)
 		{g.render_object(drawable);}
+
+		template<typename Game>
+		void rmo_impl(Game& g)
+		{}
+
+		template<typename Game, typename Head, typename... Tail>
+		void rmo_impl(Game& g, const Head& head, Tail&&... tail)
+		{
+			ro(g, head);
+			rmo_impl(g, std::forward<Tail>(tail)...);
+		}
+
+		template<typename Game, typename... Args>
+		void rmo(Game& g, Args&&... args)
+		{rmo_impl(g, std::forward<Args>(args)...);}
 	}
 }
 
