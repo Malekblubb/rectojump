1404a20a4a62254c88453ac7a57df18b72067f79
Christoph Malek <malekchristoph@gmail.com>
2014-01-19 20:29:02 +0100

    better render

--- include/rectojump/core/render.hpp
+++ include/rectojump/core/render.hpp
@@ -7,14 +7,34 @@
 #define RJ_CORE_RENDER_HPP
 
 
+#include <SFML/Graphics.hpp>
+
+
 namespace rj
 {
 	namespace rndr
 	{
+		// render a single object
 		template<typename Game, typename Drawable>
 		void ro(Game& g, const Drawable& drawable)
 		{g.render_object(drawable);}
 
+		// render helpers
+		template<typename Game, typename T, bool is_drawable>
+		struct ro_helper
+		{
+			ro_helper(Game& g, const T& obj)
+			{for(auto& a : obj) ro(g, a);}
+		};
+
+		template<typename Game, typename T>
+		struct ro_helper<Game, T, true>
+		{
+			ro_helper(Game& g, const T& obj)
+			{ro(g, obj);}
+		};
+
+		// render multiple objects impl
 		template<typename Game>
 		void rmo_impl(Game& g)
 		{ }
@@ -22,10 +42,11 @@ namespace rj
 		template<typename Game, typename Head, typename... Tail>
 		void rmo_impl(Game& g, const Head& head, Tail&&... tail)
 		{
-			ro(g, head);
+			ro_helper<Game, Head, std::is_base_of<sf::Drawable, Head>::value>{g, head};
 			rmo_impl(g, std::forward<Tail>(tail)...);
 		}
 
+		// render multiple objects interface
 		template<typename Game, typename... Args>
 		void rmo(Game& g, Args&&... args)
 		{rmo_impl(g, std::forward<Args>(args)...);}
