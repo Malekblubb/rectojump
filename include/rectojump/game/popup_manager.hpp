5ae6fb648a5632dac93323a7f305d8a02fe8ed61
Christoph Malek <malekchristoph@gmail.com>
2014-01-22 20:15:43 +0100

    added popup_creator

--- include/rectojump/game/popup_manager.hpp
+++ include/rectojump/game/popup_manager.hpp
@@ -15,6 +15,12 @@
 
 namespace rj
 {
+	enum class popup_type : char
+	{normal, info, error};
+
+	template<popup_type, typename>
+	struct popup_creator;
+
 	template<typename Game_Handler>
 	class popup_manager
 	{
@@ -42,9 +48,13 @@ namespace rj
 				a.render();
 		}
 
-		template<typename... Args>
+		template<popup_type type = popup_type::normal, typename... Args>
 		void create_popup(const std::string& text, const vec2f& pos = {settings::get_window_size<vec2f>().x / 2.f, settings::get_window_size<vec2f>().y - 100.f}, Args&&... args = {})
-		{m_popups.emplace_back(&m_game, m_font, text, pos, std::forward<Args>(args)...);}
+		{popup_creator<type, Game_Handler>{*this, text, pos, std::forward<Args>(args)...};}
+
+		template<typename... Args>
+		void create_popup_impl(Args&&... args)
+		{m_popups.emplace_back(&m_game, m_font, std::forward<Args>(args)...);}
 
 		std::size_t num_popups() const noexcept
 		{return m_popups.size();}
@@ -55,6 +65,28 @@ namespace rj
 			mlk::cnt::remove_all_if(
 			[](const popup& p){return p.is_destroyed();}, m_popups);
 		}
+	};
+
+	template<popup_type type, typename Game_Handler>
+	struct popup_creator
+	{
+		template<typename... Args>
+		popup_creator(popup_manager<Game_Handler>& pm, Args&&... args)
+		{pm.create_popup_impl(std::forward<Args>(args)...);}
+	};
+
+	template<typename Game_Handler>
+	struct popup_creator<popup_type::info, Game_Handler>
+	{
+		popup_creator(popup_manager<Game_Handler>& pm, const std::string& text, const vec2f& pos)
+		{pm.create_popup_impl(text, pos, 3000, to_rgb("#000375", 200), to_rgb("#000352"), to_rgb("#e3e3e3"));}
+	};
+
+	template<typename Game_Handler>
+	struct popup_creator<popup_type::error, Game_Handler>
+	{
+		popup_creator(popup_manager<Game_Handler>& pm, const std::string& text, const vec2f& pos)
+		{pm.create_popup_impl(text, pos, 3000, to_rgb("#820006", 200), to_rgb("#d80042"), to_rgb("#e3e3e3"));}
 	};
 }
 
