7b468c2cfe38ca273989f102e30b25dbdc84266a
Christoph Malek <malekchristoph@gmail.com>
2014-01-23 19:55:49 +0100

    added editor class

---
+++ include/rectojump/game/editor/editor.hpp
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2013-2014 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_GAME_EDITOR_EDITOR_HPP
+#define RJ_GAME_EDITOR_EDITOR_HPP
+
+
+namespace rj
+{
+	class editor
+	{
+
+	};
+}
+
+
+
+#endif // RJ_GAME_EDITOR_EDITOR_HPP
