f01068b504cbab124d5e6414a879f3483ffb4fad
Christoph Malek <malekchristoph@gmail.com>
2014-02-02 17:13:37 +0100

    added entity_figure::f_triangles4

--- include/rectojump/game/entity_groups.hpp
+++ include/rectojump/game/entity_groups.hpp
@@ -19,7 +19,12 @@ namespace rj
 	{moveable_left, jumpable, solid, death, num};
 
 	enum entity_figure : char
-	{f_rectangle, f_triangle, f_num};
+	{
+		f_rectangle,
+		f_triangle,
+		f_triangles4,
+		f_num
+	};
 
 	using etypes = mlk::ebitset<entity_type, entity_type::num>;
 	using eprops = mlk::ebitset<entity_propertie, entity_propertie::num>;
