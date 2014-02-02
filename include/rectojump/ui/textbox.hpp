1992ed3a15b1dd575aff3f1dd96fe4921fcad09a
Christoph Malek <malekchristoph@gmail.com>
2014-02-02 14:22:40 +0100

    added ui::textbox class

---
+++ include/rectojump/ui/textbox.hpp
@@ -0,0 +1,173 @@
+//
+// Copyright (c) 2013-2014 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_UI_TEXTBOX_HPP
+#define RJ_UI_TEXTBOX_HPP
+
+
+#include <rectojump/core/game_window.hpp>
+#include <rectojump/global/common.hpp>
+#include <rectojump/shared/input.hpp>
+#include <rectojump/shared/utils.hpp>
+
+#include <mlk/time/simple_timer.h>
+
+
+namespace rj
+{
+	namespace ui
+	{
+		class textbox : public sf::Drawable, public sf::Transformable
+		{
+			game_window& m_gamewindow;
+			sf::RectangleShape m_shape;
+			sf::Text m_text;
+			sf::VertexArray m_cursor{sf::Lines};
+			sf::Color m_cursorcolor{sf::Color::Black};
+
+			mlk::ullong m_cursor_blinkinterval{700};
+			mlk::tm::simple_timer m_blinktimer{m_cursor_blinkinterval};
+			bool m_focus{false}, m_cursor_visible{true};
+
+		public:
+			textbox(game_window& gw, const vec2f& size, const vec2f& pos, const sf::Font& font, const std::string& text = "") :
+				m_gamewindow{gw},
+				m_shape{size},
+				m_text{text, font}
+			{this->setPosition(pos); this->init();}
+
+			void update(dur)
+			{
+				if(is_btn_pressed(btn::Left))
+				{
+					auto shape_bounds(bounds_from_vec(this->getPosition(), m_shape.getSize()));
+					if(shape_bounds.intersects(get_mousebounds<true>()))
+						m_focus = true;
+					else m_focus = false;
+				}
+				if(!m_focus)
+					return;
+
+				if(m_blinktimer.timed_out())
+				{
+					m_cursor_visible = !m_cursor_visible;
+					m_blinktimer.restart();
+				}
+			}
+
+			// sfml-func-style interface
+			// setters
+			void setSize(const vec2f& size) noexcept
+			{m_shape.setSize(size); this->update_cursor(); this->update_text_pos();}
+
+			void setFillColor(const sf::Color& color) noexcept
+			{m_shape.setFillColor(color);}
+
+			void setOutlineColor(const sf::Color& color) noexcept
+			{m_shape.setOutlineColor(color);}
+
+			void setOutlineThickness(float thickness) noexcept
+			{m_shape.setOutlineThickness(thickness);}
+
+			void setCursorBlinkInterval(mlk::ullong interval) noexcept
+			{m_cursor_blinkinterval = interval;}
+
+			void setCursorColor(const sf::Color& color) noexcept
+			{m_cursorcolor = color; this->update_cursor();}
+
+			void setTextColor(const sf::Color& color) noexcept
+			{m_text.setColor(color);}
+
+			void setTextSize(mlk::uint size) noexcept
+			{m_text.setCharacterSize(size); this->update_text_pos();}
+
+			// getters
+			const vec2f& getSize(const vec2f& size) const noexcept
+			{return m_shape.getSize();}
+
+			const sf::Color& getFillColor() const noexcept
+			{return m_shape.getFillColor();}
+
+			const sf::Color& getOutlineColor() const noexcept
+			{return m_shape.getOutlineColor();}
+
+			float getOutlineThickness() const noexcept
+			{return m_shape.getOutlineThickness();}
+
+			mlk::ullong getCursorBlinkInterval() const noexcept
+			{return m_cursor_blinkinterval;}
+
+			const sf::Color& getCursorColor() const noexcept
+			{return m_cursorcolor;}
+
+			const sf::Color& getTextColor() const noexcept
+			{return m_text.getColor();}
+
+			mlk::uint getTextSize() const noexcept
+			{return m_text.getCharacterSize();}
+
+		private:
+			void init() noexcept
+			{
+				m_cursor.append({{0.f, 0.f}, m_cursorcolor});
+				m_cursor.append({{0.f, 0.f}, m_cursorcolor});
+				this->update_cursor();
+				this->update_text_pos();
+
+				// bind the event
+				m_gamewindow.on_event(sf::Event::TextEntered) +=
+				[this](sf::Event e){if(!m_focus) return; this->update_text(e.text.unicode);};
+
+				m_blinktimer.run();
+			}
+
+			void draw(sf::RenderTarget &target, sf::RenderStates states) const override
+			{
+				auto s(states);
+				s.transform = sf::Transformable::getTransform();
+				target.draw(m_shape, s);
+				target.draw(m_text, s);
+				if(m_cursor_visible && m_focus)
+					target.draw(m_cursor, s);
+			}
+
+			void update_text(std::uint32_t u)
+			{
+				auto text(m_text.getString());
+				if(u == 8) // backspace
+				{
+					if(!text.isEmpty())
+						text.erase(text.getSize() - 1, 1);
+				}
+				else text += u;
+
+				m_text.setString(text);
+				this->update_cursor();
+			}
+
+			void update_text_pos()
+			{
+				auto shape_bounds(m_shape.getGlobalBounds());
+				auto text_bounds(m_text.getGlobalBounds());
+				m_text.setOrigin(vec2f{0.f, text_bounds.height} / 2.f);
+				m_text.setPosition(shape_bounds.left, shape_bounds.top + shape_bounds.height / 2.f);
+			}
+
+			void update_cursor()
+			{
+				auto shape_bounds(m_shape.getGlobalBounds());
+				auto text_bounds(m_text.getGlobalBounds());
+				auto cursor_margin(shape_bounds.height - (0.9f * shape_bounds.height));
+				m_cursor[0].position = {shape_bounds.left + text_bounds.width, shape_bounds.top + cursor_margin};
+				m_cursor[0].color = m_cursorcolor;
+				m_cursor[1].position = {shape_bounds.left + text_bounds.width, shape_bounds.top + shape_bounds.height - cursor_margin};
+				m_cursor[1].color = m_cursorcolor;
+			}
+		};
+	}
+}
+
+
+#endif // RJ_UI_TEXTBOX_HPP
