65c363335dc50b60df6b3756dd32cad7df5aa3ca
Christoph Malek <malekchristoph@gmail.com>
2014-02-07 19:32:29 +0100

    debug_info: reworked the way how the text is generated

--- include/rectojump/game/debug_info.hpp
+++ include/rectojump/game/debug_info.hpp
@@ -11,11 +11,53 @@
 #include <rectojump/global/common.hpp>
 #include <rectojump/shared/debug_text.hpp>
 
+#include <mlk/tools/compiletime.h>
 #include <mlk/tools/stl_string_utl.h>
 
 
 namespace rj
 {
+	struct debug_info_string_base
+	{
+		virtual ~debug_info_string_base() = default;
+		virtual std::string get_formated() const = 0;
+	};
+
+	template<typename Func_Ret_Type>
+	class debug_info_string : public debug_info_string_base
+	{
+		const std::string m_unformated_str;
+		std::function<Func_Ret_Type()> m_func;
+
+	public:
+		template<typename Func>
+		debug_info_string(const std::string& unformated_str, Func&& f) :
+			m_unformated_str{unformated_str},
+			m_func{f}
+		{ }
+
+		~debug_info_string() = default;
+
+		std::string get_formated() const override
+		{
+			auto result(m_unformated_str);
+			auto str(std::to_string(m_func()));
+			auto insert_pos(result.find("%%"));
+			if(insert_pos != std::string::npos)
+			{
+				result.insert(insert_pos + 2, str);
+				result.erase(insert_pos, 2);
+			}
+			return result;
+		}
+	};
+
+	template<>
+	inline std::string debug_info_string<void>::get_formated() const
+	{return m_unformated_str;}
+
+
+	// debug_info
 	template<typename Game_Handler, typename Game>
 	class debug_info
 	{
@@ -25,6 +67,7 @@ namespace rj
 
 		sf::RectangleShape m_background{{100.f, 100.f}};
 		debug_text m_text;
+		std::vector<mlk::sptr<debug_info_string_base>> m_lines;
 
 	public:
 		debug_info(Game_Handler& gh) :
@@ -32,30 +75,57 @@ namespace rj
 			m_render{gh.get_render()},
 			m_game{gh.get_game()},
 			m_text{gh.get_datamgr()}
+		{this->init();}
+
+		void update(dur)
+		{
+			m_background.setSize(m_text.get_size());
+			std::string display_text;
+			for(auto& a : m_lines)
+				display_text += a->get_formated();
+			m_text.set_text(display_text);
+		}
+
+		void render()
+		{m_render(m_background, m_text);}
+
+	private:
+		void init() noexcept
 		{
 			m_background.setPosition({1.f, 1.f});
 			m_background.setFillColor({255, 150, 123, 220});
 			m_background.setOutlineThickness(1);
 			m_background.setOutlineColor({255, 0, 0});
+
+			this->add_title_line("Performance");
+			this->add_line("FPS:        %%", [this]{return m_gamehandler.get_gamewindow().get_updater().get_fps();});
+			this->add_line("FDur:       %%\n", [this]{return m_gamehandler.get_gamewindow().get_updater().get_frameduration();});
+
+			this->add_title_line("Components");
+			this->add_line("Gameworld:  %%", [this]{return m_game.get_world().num_entities();});
+			this->add_line("Background: %%", [this]{return m_gamehandler.get_backgroundmgr().num_components();});
+			this->add_line("Popups:     %%\n", [this]{return m_gamehandler.get_popupmgr().num_popups();});
+
+			this->add_title_line("Managers");
+			this->add_line("Data:       %%", [this]{return m_gamehandler.get_datamgr().num_data();});
+			this->add_line("Levels:     %%", [this]{return m_gamehandler.get_levelmgr().num_levels();});
 		}
 
-		void update(dur)
+		void add_title_line(const std::string& text)
 		{
-			m_background.setSize(m_text.get_size());
-			auto text(mlk::stl_string::str_format(
-						  "Performance\n===========\nFPS:        %%\nFDur:       %%\n\nComponents\n==========\nGameworld:  %%\nBackground: %%\nPopups:     %%\n\nManagers\n========\nData:       %%\nLevels:     %%",
-						  m_gamehandler.get_gamewindow().get_updater().get_fps(),
-						  m_gamehandler.get_gamewindow().get_updater().get_frameduration(),
-						  m_game.get_world().num_entities(),
-						  m_gamehandler.get_backgroundmgr().num_components(),
-						  m_gamehandler.get_popupmgr().num_popups(),
-						  m_gamehandler.get_datamgr().num_data(),
-						  m_gamehandler.get_levelmgr().num_levels()));
-			m_text.set_text(text);
+			std::string app{'\n'};
+			for(auto& a : text)
+				app += "=";
+			auto ptr(std::make_shared<debug_info_string<void>>(std::string{text + app + "\n"}, []{}));
+			m_lines.emplace_back(ptr);
 		}
 
-		void render()
-		{m_render(m_background, m_text);}
+		template<typename Func>
+		void add_line(const std::string& text, Func&& f)
+		{
+			auto ptr(std::make_shared<debug_info_string<typename mlk::lambda_info<Func>::return_type>>(text + '\n', f));
+			m_lines.emplace_back(ptr);
+		}
 	};
 }
 
