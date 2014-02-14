503a6cef853e5c8ddb3046b9d26d18d5cd409fe2
Christoph Malek <malekchristoph@gmail.com>
2014-02-14 19:59:36 +0100

    game_menu: done basic structure

--- include/rectojump/game/game_menu/game_menu.hpp
+++ include/rectojump/game/game_menu/game_menu.hpp
@@ -7,12 +7,36 @@
 #define RJ_GAME_GAME_MENU_GAME_MENU_HPP
 
 
+#include <rectojump/global/common.hpp>
+
+
 namespace rj
 {
 	template<typename Game_Handler>
 	class game_menu
 	{
+		Game_Handler& m_gamehandler;
 
+	public:
+		game_menu(Game_Handler& gh) :
+			m_gamehandler{gh}
+		{this->init();}
+
+		void update(dur duration)
+		{
+
+		}
+
+		void render()
+		{
+
+		}
+
+	private:
+		void init()
+		{
+
+		}
 	};
 }
 
