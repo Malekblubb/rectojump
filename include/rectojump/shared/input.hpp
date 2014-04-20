cdd08acfe4340496541762c9c3ec228c78276c86
Christoph Malek <malekchristoph@gmail.com>
2014-04-20 14:49:25 +0200

    input: removed all auto returns added inp namespace for helper funcs

--- include/rectojump/shared/input.hpp
+++ include/rectojump/shared/input.hpp
@@ -18,6 +18,24 @@
 
 namespace rj
 {
+	// forward helper funcs
+	namespace inp
+	{
+		mlk::slot<>& on_input_update();
+		char get_last_textinput();
+		mlk::slot<>& on_key_pressed(key k);
+		template<typename... Keys> mlk::slot<>& on_keys_pressed(Keys&&...);
+		bool is_key_pressed(key);
+		void simulate_keypress(key);
+		mlk::slot<const vec2f&>& on_btn_pressed(btn b);
+		bool is_btn_pressed(btn);
+		void simulate_btnpress(btn);
+		mlk::slot<const vec2f&>& on_mousewheel(wheel w);
+		vec2f& get_mousepos();
+		vec2f& get_lastmousepos();
+		vec2f get_mousepos_current_view();
+	}
+
 	using key_vec = std::vector<key>;
 
 	class game_window;
@@ -130,93 +148,96 @@ namespace rj
 		{delta < 0 ? m_on_mousewheel[wheel::down](m_mousepos) : m_on_mousewheel[wheel::up](m_mousepos);}
 
 
-		friend auto& on_input_update();
-		friend char get_last_textinput();
-		friend auto& on_key_pressed(key k);
+		friend mlk::slot<>& inp::on_input_update();
+		friend char inp::get_last_textinput();
+		friend mlk::slot<>& inp::on_key_pressed(key k);
 		template<typename... Keys>
-		friend mlk::slot<>& on_keys_pressed(Keys&&...);
-		friend bool is_key_pressed(key);
-		friend void simulate_keypress(key);
-		friend auto& on_btn_pressed(btn b);
-		friend bool is_btn_pressed(btn);
-		friend void simulate_btnpress(btn);
-		friend auto& on_mousewheel(wheel w);
-		friend auto& get_mousepos();
-		friend auto& get_lastmousepos();
-		friend vec2f get_mousepos_current_view();
+		friend mlk::slot<>& inp::on_keys_pressed(Keys&&...);
+		friend bool inp::is_key_pressed(key);
+		friend void inp::simulate_keypress(key);
+		friend mlk::slot<const vec2f&>& inp::on_btn_pressed(btn b);
+		friend bool inp::is_btn_pressed(btn);
+		friend void inp::simulate_btnpress(btn);
+		friend mlk::slot<const vec2f&>& inp::on_mousewheel(wheel w);
+		friend vec2f& inp::get_mousepos();
+		friend vec2f& inp::get_lastmousepos();
+		friend vec2f inp::get_mousepos_current_view();
 	};
 
-	inline auto& on_input_update()
-	{return input<game_window>::get().m_on_update;}
-
-	inline char get_last_textinput()
-	{return input<game_window>::get().m_last_textinput;}
-
-	inline auto& on_key_pressed(key k)
+	namespace inp
 	{
-		if(!input<game_window>::get().is_key_valid(k))
-			throw std::runtime_error{"invalid key passed"};
-		return input<game_window>::get().m_on_key_pressed[k];
-	}
+		inline mlk::slot<>& on_input_update()
+		{return input<game_window>::get().m_on_update;}
 
-	template<typename... Keys>
-	mlk::slot<>& on_keys_pressed(Keys&&... keys)
-	{
-		key_vec keys_vec{std::forward<Keys>(keys)...};
-		return input<game_window>::get().m_on_keys_pressed[keys_vec];
-	}
+		inline char get_last_textinput()
+		{return input<game_window>::get().m_last_textinput;}
 
-	inline bool is_key_pressed(key k)
-	{return input<game_window>::get().m_key_bits & k;}
+		inline mlk::slot<>& on_key_pressed(key k)
+		{
+			if(!input<game_window>::get().is_key_valid(k))
+				throw std::runtime_error{"invalid key passed"};
+			return input<game_window>::get().m_on_key_pressed[k];
+		}
 
-	inline void simulate_keypress(key k)
-	{
-		input<game_window>::get().key_pressed(k);
-		input<game_window>::get().key_released(k);
-	}
+		template<typename... Keys>
+		mlk::slot<>& on_keys_pressed(Keys&&... keys)
+		{
+			key_vec keys_vec{std::forward<Keys>(keys)...};
+			return input<game_window>::get().m_on_keys_pressed[keys_vec];
+		}
 
-	inline auto& on_btn_pressed(btn b)
-	{return input<game_window>::get().m_on_btn_pressed[b];}
+		inline bool is_key_pressed(key k)
+		{return input<game_window>::get().m_key_bits & k;}
 
-	inline bool is_btn_pressed(btn b)
-	{return input<game_window>::get().m_mousebtn_bits & b;}
+		inline void simulate_keypress(key k)
+		{
+			input<game_window>::get().key_pressed(k);
+			input<game_window>::get().key_released(k);
+		}
 
-	inline void simulate_btnpress(btn b)
-	{
-		input<game_window>::get().btn_pressed(b);
-		input<game_window>::get().btn_released(b);
-	}
+		inline mlk::slot<const vec2f&>& on_btn_pressed(btn b)
+		{return input<game_window>::get().m_on_btn_pressed[b];}
 
-	inline auto& on_mousewheel(wheel w)
-	{return input<game_window>::get().m_on_mousewheel[w];}
+		inline bool is_btn_pressed(btn b)
+		{return input<game_window>::get().m_mousebtn_bits & b;}
 
-	inline auto& get_mousepos()
-	{return input<game_window>::get().m_mousepos;}
+		inline void simulate_btnpress(btn b)
+		{
+			input<game_window>::get().btn_pressed(b);
+			input<game_window>::get().btn_released(b);
+		}
 
-	inline auto& get_lastmousepos()
-	{return input<game_window>::get().m_last_mousepos;}
+		inline mlk::slot<const vec2f&>& on_mousewheel(wheel w)
+		{return input<game_window>::get().m_on_mousewheel[w];}
 
-	inline vec2f get_mousepos_current_view()
-	{
-		auto& rw(input<game_window>::get().m_renderwindow);
-		return rw->mapPixelToCoords(sf::Mouse::getPosition(*rw), rw->getView());
-	}
+		inline vec2f& get_mousepos()
+		{return input<game_window>::get().m_mousepos;}
 
-	inline vec2f get_mousepos(const sf::RenderWindow& rw, const sf::View& v)
-	{return rw.mapPixelToCoords(sf::Mouse::getPosition(rw), v);}
+		inline vec2f& get_lastmousepos()
+		{return input<game_window>::get().m_last_mousepos;}
 
-	template<bool current_view = false>
-	sf::FloatRect get_mousebounds()
-	{
-		auto& pos(get_mousepos());
-		return {pos.x, pos.y, 1.f, 1.f};
-	}
+		inline vec2f get_mousepos_current_view()
+		{
+			auto& rw(input<game_window>::get().m_renderwindow);
+			return rw->mapPixelToCoords(sf::Mouse::getPosition(*rw), rw->getView());
+		}
 
-	template<>
-	inline sf::FloatRect get_mousebounds<true>()
-	{
-		auto pos(get_mousepos_current_view());
-		return {pos.x, pos.y, 1.f, 1.f};
+		inline vec2f get_mousepos(const sf::RenderWindow& rw, const sf::View& v)
+		{return rw.mapPixelToCoords(sf::Mouse::getPosition(rw), v);}
+
+		template<bool current_view = false>
+		sf::FloatRect get_mousebounds()
+		{
+			auto& pos(get_mousepos());
+			return {pos.x, pos.y, 1.f, 1.f};
+		}
+
+		template<>
+		inline sf::FloatRect get_mousebounds<true>()
+		{
+			auto pos(get_mousepos_current_view());
+			return {pos.x, pos.y, 1.f, 1.f};
+		}
 	}
 }
 
