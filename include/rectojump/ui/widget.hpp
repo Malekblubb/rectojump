5505b14f54a091991100af64b285c7696140cf5b
Christoph Malek <malekchristoph@gmail.com>
2014-04-26 10:40:07 +0200

    added widget

---
+++ include/rectojump/ui/widget.hpp
@@ -0,0 +1,30 @@
+//
+// Copyright (c) 2013-2014 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef RJ_UI_WIDGET_HPP
+#define RJ_UI_WIDGET_HPP
+
+
+#include <rectojump/global/common.hpp>
+
+
+namespace rj
+{
+	namespace ui
+	{
+		class widget : public sf::Drawable
+		{
+		public:
+			virtual void update(dur)
+			{ }
+
+			virtual void render()
+			{ }
+		};
+	}
+}
+
+
+#endif // RJ_UI_WIDGET_HPP
