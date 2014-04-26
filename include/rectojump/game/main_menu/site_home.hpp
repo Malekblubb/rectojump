6ee7146be9b4730669e772d771cc6553b790eb90
Christoph Malek <malekchristoph@gmail.com>
2014-04-26 10:44:18 +0200

    game > main_menu > site_home: adapted to sf_widget

--- include/rectojump/game/main_menu/site_home.hpp
+++ include/rectojump/game/main_menu/site_home.hpp
@@ -7,6 +7,7 @@
 #define RJ_GAME_MAIN_MENU_SITE_HOME_HPP
 
 
+#include <rectojump/ui/sf_widget.hpp>
 #include <rectojump/ui/stacked_widget.hpp>
 
 
@@ -16,19 +17,28 @@ namespace rj
 	class site_home
 	{
 		Overlay& m_overlay;
+		data_store_type& m_datasore;
+
 		ui::stacked_widget& m_sites;
 
 	public:
 		site_home(Overlay& ov) :
 			m_overlay{ov},
+			m_datasore{m_overlay.mainmenu().gamehandler().datastore()},
 			m_sites{ov.sites()}
 		{ }
 
 		void construct()
 		{
-			auto logo_shape(m_sites.add_object<sf::RectangleShape>("home", vec2f{128, 128}));
-			logo_shape->setTexture(&m_overlay.mainmenu().gamehandler().datastore().template get<sf::Texture>("rj_logo.png"));
-			logo_shape->setPosition({(m_sites.bounds().width - logo_shape->getSize().x) / 2.f, 50.f});
+			auto logo_shape(m_sites.add_object<widget::rectangle_shape>("home", vec2f{128, 128}));
+			logo_shape->get().setTexture(&m_datasore.template get<sf::Texture>("rj_logo.png"));
+			logo_shape->get().setPosition({(m_sites.bounds().width - logo_shape->get().getSize().x) / 2.f, 50.f});
+
+			auto username(m_sites.add_object<ui::textbox>("home", vec2f{150.f, 50.f}, vec2f{20, 20}, m_datasore.template get<sf::Font>(glob::text_font), "text"));
+			username->setOutlineThickness(2.f);
+			username->setOutlineColor(sf::Color::Red);
+			username->setTextColor(sf::Color::Green);
+			username->setCursorColor(sf::Color::Green);
 		}
 	};
 }
