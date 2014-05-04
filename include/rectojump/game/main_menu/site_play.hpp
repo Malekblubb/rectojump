5cd7ce852fb6ff9460050d1f0a02e1e4c19dac4d
Christoph Malek <malekchristoph@gmail.com>
2014-05-04 14:25:40 +0200

    game > main_menu > site_play: adding levels added scroll input

--- include/rectojump/game/main_menu/site_play.hpp
+++ include/rectojump/game/main_menu/site_play.hpp
@@ -35,10 +35,30 @@ namespace rj
 			auto& site_play(m_sites.get("play"));
 			auto& font(m_overlay.mainmenu().gamehandler().datastore().template get<sf::Font>(settings::text_font()));
 
+			// create level_widget
 			auto level_widget(m_sites.add_object<ui::level_widget>("play", m_overlay.mainmenu().gamehandler().gamewindow(), m_sites.size(), m_sites.pos()));
-			level_widget->add_item(m_lvmgr.get_level("test.rjl"), font);
 
+			// add levels
+			for(const auto& a : m_lvmgr.get_levels())
+				level_widget->add_item(a.second, font);
+
+			// set camera on render
 			site_play.on_render = [level_widget]{level_widget->activate_cam();};
+
+			// add input
+			m_overlay.mainmenu().gamehandler().template add_input<state::main_menu>
+			([this, level_widget](const vec2f&)
+			{
+				if(m_sites.active("play"))
+					level_widget->scroll_up();
+			}, wheel::up);
+
+			m_overlay.mainmenu().gamehandler().template add_input<state::main_menu>
+			([this, level_widget](const vec2f&)
+			{
+				if(m_sites.active("play"))
+					level_widget->scroll_down();
+			}, wheel::down);
 		}
 	};
 }
